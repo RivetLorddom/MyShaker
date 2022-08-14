383b71cac243e5c885641e842d5e5e78c15de9b1
Rivet Lorddom <er@Rivets-MacBook-Pro.local>
2022-08-15 00:17:50 +0900

    facicoon and User model initialized

--- MyShaker/settings.py
+++ MyShaker/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+from ....project4.project4.settings import AUTH_USER_MODEL
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -81,6 +83,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = "shaker.User"
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators

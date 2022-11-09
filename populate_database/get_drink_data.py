e1d9810b651bafdf4a2f2f8f3de43fd7bd6f26ed
Riv <rivet.tmr@gmail.com>
2022-11-10 02:24:41 +0900

    checking debug

--- populate_database/get_drink_data.py
+++ populate_database/get_drink_data.py
@@ -1,10 +1,7 @@
 from shaker.models import Drink
 import json
 
-
 with open('populate_database/drink_data.json', encoding='utf-8') as data_file:
     json_data = json.loads(data_file.read())
-
     for drink_data in json_data["drinks"]:
         drink = Drink.create(**drink_data)
-       

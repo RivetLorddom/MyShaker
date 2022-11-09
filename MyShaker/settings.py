be9e94f3f40c9aea12d98e4e388321355b22b347
Riv <rivet.tmr@gmail.com>
2022-11-10 02:29:47 +0900

    turning debug off

--- MyShaker/settings.py
+++ MyShaker/settings.py
@@ -36,7 +36,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG", default=True)
+DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
